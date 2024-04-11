c6e99601f99ff749848fe254b78b6c85b04a61ff
Genezi-00 <genesis@kaztro.cloud>
2024-04-11 15:43:43 -0500

    subiendo repositorio de Examen

---
+++ src/main/java/com/examenCl1/Examen/DawiCl1WilmerOcampoApplication.java
@@ -0,0 +1,13 @@
+package com.examenCl1.Examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1WilmerOcampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1WilmerOcampoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examenCl1/Examen/DawiCl1WilmerOcampoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examenCl1.Examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1WilmerOcampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
